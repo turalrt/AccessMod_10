c50b4e9cd7b6fcb50ed9eed2e0ad0f003f479744
Tural Taghiyev <turalrt@code.edu.az>
2021-12-05 23:28:26 +0400

    first commit

---
+++ Reverse/ReverseCode.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse
+{
+    internal class ReverseCode
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(Code("Hello World"));
+        }
+        public static string Code(string str)
+        {
+            string result = string.Empty;
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                result+=str[i];
+            }
+            return(result);
+        }
+    }
+}
